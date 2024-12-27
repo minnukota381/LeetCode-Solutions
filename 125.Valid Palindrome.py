4144b42a117fdf85ac1bf73ee3d63dda8c1b0555
minnukota381 <minnukota381@gmail.com>
2024-12-27 15:04:39 +0530

    Refactor isPalindrome method for improved readability and efficiency

--- 125.Valid Palindrome.py
+++ 125.Valid Palindrome.py
@@ -1,15 +1,28 @@
+# class Solution(object):
+#     def isPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         empty=[]
+#         for i in s:
+#             if i.isalnum():
+#                 empty.append(i.lower())
+#         new = "".join(empty)
+
+#         return new==new[::-1]
+
 class Solution(object):
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        empty=[]
-        for i in s:
-            if i.isalnum():
-                empty.append(i.lower())
-        new = "".join(empty)
+        st=""
+        s=s.lower()
+        for x in s:
+            if x.isalnum():
+                st+=x
 
-        return new==new[::-1]
-
-        
+        return st==st[::-1]
+          
