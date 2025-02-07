b7620f82903e5049eff33e8f4e332eeab2eece79
minnukota381 <minnukota381@gmail.com>
2025-02-07 11:22:09 +0530

    Add implementation for singleNumber method in Solution class

---
+++ 137. Single Number II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # for i in nums:
+        #     if nums.count(i)==1:
+        #         return i
+        # return None
+
+        d=Counter(nums)
+        for i in nums:
+            if d[i]==1:
+                return i
