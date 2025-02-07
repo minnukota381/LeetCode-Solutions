45117299bf640c9c3856a95d4670e59dcd6923f3
minnukota381 <minnukota381@gmail.com>
2025-02-07 12:25:32 +0530

    415. Add Strings.py | Add implementation for addStrings method in Solution class

---
+++ 415. Add Strings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+
+        num1=int(num1)
+        num2=int(num2)
+
+        return str(num1+num2)
+        
